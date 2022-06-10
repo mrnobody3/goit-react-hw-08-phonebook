2daf50e5542aebd1eab65f16ee5adf25430c2c59
mrnobody3 <91002309+mrnobody3@users.noreply.github.com>
2022-06-10 10:28:53 +0000

    Deploying to gh-pages from @ mrnobody3/goit-react-hw-08-phonebook@7a4a83686b90b22d012ed6777950cc84160af5ce 🚀

---
+++ static/js/35.964de56b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[35],{2035:function(o,e,n){n.r(e),n.d(e,{default:function(){return u}});var t=n(7022),c=n(5736),r=n(3329),u=function(){return(0,r.jsx)(t.Z,{children:(0,r.jsx)(c.Z,{bg:"info",as:"h1",className:"d-block mx-auto fs-1 mt-5",children:"Welcome to your Phonebook"})})}}}]);
+//# sourceMappingURL=35.964de56b.chunk.js.map
